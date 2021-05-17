7b4e87c2cb6fc461850671d12b3f75ab04a942ed
Yara Sá <yarasasantana@gmail.com>
2021-05-17 19:10:51 -0300

    primeira alteração

---
+++ lib/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contador'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'Contagem: $count',
+          style: TextStyle(fontSize: 18, color: Colors.white),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu Primeiro App',
+      home: HomePage(),
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+        scaffoldBackgroundColor: Colors.purpleAccent,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.purpleAccent,
+        ),
+      ),
+      darkTheme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.grey,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.grey,
+        ),
+      ),
+    );
+  }
+}
